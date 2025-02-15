035c7dcd797f75cb4743df0dd73c5770896918f5
H25639SRV <giahuytruong2506@gmail.com>
2025-02-15 18:35:05 +0700

    resort

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import { createClient } from "contentful";
+
+export default createClient({
+  space: "jwzpwmeseued",
+  accessToken: "gc_Wr32d6Mby0eqyWrS7txAPbkOtBHo--grH29JwFj8",
+});
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+export default function Home() {
+  return (
+    <>
+      <Hero>
+        <Banner title="luxury rooms" subtitle="deluxe rooms starting at 2m vnđ">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+}
